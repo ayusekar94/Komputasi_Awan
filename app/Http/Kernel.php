08651b039033f50599d64d24a7608e1dbf10a75c
ayusekar94 <ayu.wiranti181@gmail.com>
2022-12-26 11:15:43 +0700

    kernel

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,5 +63,9 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'admin' => \App\Http\Middleware\Admin::class,
+        'login' => \App\Http\Middleware\Login::class,
+        'dokter' => \App\Http\Middleware\Dokter::class,
+        'apoteker' => \App\Http\Middleware\Dokter::class,
     ];
 }
